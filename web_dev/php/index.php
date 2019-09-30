71f340c902410f3827f1746bcca6514a3e88a430
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-09-30 22:51:07 +0530

    php mysql container web_dev

---
+++ web_dev/php/index.php
@@ -0,0 +1,16 @@
+<?php
+   $host = 'db'; //service name from docker-compose.yml
+   $user = 'devuser';
+   $password = 'devpass';
+   $db = 'test_database';
+ 
+   $conn = new mysqli($host,$user,$password,$db);
+   if ($conn->connect_error){
+     echo "Unable To Connect With Database".$conn->connect_error;
+     exit;
+   }
+   echo "Successfully Connected To MySQL Database";
+                                                                                                                     
+   echo "Changes are effecting ....";
+?>
+
